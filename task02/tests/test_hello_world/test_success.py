d8de404f6a6048dc4d977d8d6a2e0d4b72863a01
yrashan <ravishan.learn@gmail.com>
2024-05-31 17:03:26 +0530

    Task02 - Fixed test case for success

--- task02/tests/test_hello_world/test_success.py
+++ task02/tests/test_hello_world/test_success.py
@@ -4,5 +4,13 @@ from tests.test_hello_world import HelloWorldLambdaTestCase
 class TestSuccess(HelloWorldLambdaTestCase):
 
     def test_success(self):
-        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), 200)
+        event = {
+            'rawPath': '/hello',
+            'requestContext': {
+                'http': {
+                    'method': 'GET'
+                }
+            }
+        }
+        self.assertEqual(self.HANDLER.handle_request(event, dict()), 200)
 
